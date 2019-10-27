e6fc5cccb157f9c7cab6d3c89044ab143dcafbd1
GustavoYamauchi <gyxgames31@gmail.com>
2019-10-27 18:07:25 -0300

    Finalizando as Ado

--- ADO.sql
+++ ADO.sql
@@ -6,8 +6,21 @@ group by publishers.pub_name
 
 
 -- 2) Quais os livros que tem preço superior à média de preços do seu tipo?
+select title, price, type 
+from titles
+where price >(select type from titles where type not like (select type, avg(price) media from titles group by type)) 
 
+select type, avg(price)
+from titles
+group by type
 
+select title, price from titles where type like 'business'
+
+select title, price, type
+from titles
+where price > (SELECT AVG(t1.price)
+                FROM titles t1 CROSS JOIN titles t2
+                where t1.type = t2.type)
 -- 3) Que autores vivem na mesma cidade?  
 
 select  a1.au_fname, a2.au_fname, a1.city
@@ -38,8 +51,29 @@ insert jobs
 (job_desc, min_lvl, max_lvl)
 values ('Job' , 170, 250)
 -- 7) Insira você mesmo na tabela Employee, com o código SNC10101M, associado ao cargo que acabou de inserir no exercício 6. Não específique uma editora nem uma data de contratação.(mude o 4 primeiros dígitos numéricos para outros números)
--- 8) Reduza em 1% o preço dos livros das editoras que não são da california.
--- 9) Exclua o Employees que você criou.
--- 10) Crie uma tabela chamada "TituloFulano" (onde fulano é seu nome) com os campos: Id, Titulo, Preço, Tipo, NomeEditora e DataPublicacao, com os dados vindos das tabelas titles e publishers.
+insert employee
+(emp_id, fname, minit, lname, job_id, job_lvl)
+values('SNC77772M', 'Gustavo', '', 'Yamauchi', 15, 250)
 
+
+-- 8) Reduza em 1% o preço dos livros das editoras que não são da california.
+SELECT * FROM titles
+select * from publishers
+
+update titles
+set price = price * 0.99
+from publishers left join titles
+on publishers.pub_id = titles.pub_id
+where publishers.state <>like 'CA'
+-- 9) Exclua o Employees que você criou.
+
+delete employee
+where emp_id like 'SNC77771M'
+
+
+-- 10) Crie uma tabela chamada "TituloFulano" (onde fulano é seu nome) com os campos: Id, Titulo, Preço, Tipo, NomeEditora e DataPublicacao, com os dados vindos das tabelas titles e publishers.
+SELECT title_id Id, title Titulo, price Preco, pub_name Editora, pubdate DataPublicacao
+INTO tituloGustavo
+FROM titles LEFT JOIN publishers
+ON titles.pub_id = publishers.pub_id
 
